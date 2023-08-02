09bb16c642e1cd4a9372c0b47713e12915cb3a35
Artur Mukhatyanov <AMukhatyanov@sportinglifegroup.ca>
2023-08-02 14:58:15 -0400

    Tabs update for Skills Section

--- js/scripts.js
+++ js/scripts.js
@@ -78,8 +78,78 @@ document.addEventListener('DOMContentLoaded', function () {
   }); // end of DOMContentLoaded
 
 
-  
-  
+  // code that switches between pressed tabs and their content
+    const tabs = document.getElementsByClassName("tabcontent");
+    const tabButtons = document.getElementsByClassName("tab");
+    
+    function changeTab(n, el) {
+      // first get the current tab
+      let oldTab = document.querySelector('.tab.active');
+
+        for(var i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove("active");
+            tabButtons[i].classList.remove("active");
+        }
+        tabs[n].classList.add("active");
+        el.classList.add("active");
+
+        
+        moveIndicator(oldTab, el);
+    }
+
+
+    // move underline indicator
+    function moveIndicator(oldTab, newTab) {
+      let tabsContainer = document.querySelector('.tabs'); // Select the container
+      const newTabPosition = oldTab.compareDocumentPosition(newTab);
+      
+      const newTabWidth = newTab.offsetWidth / tabsContainer.offsetWidth;
+      let transitionWidth;
+
+      // if the new tab is to the right
+      if(newTabPosition === 4) {
+        transitionWidth = newTab.offsetLeft + newTab.offsetWidth - oldTab.offsetLeft;
+      } else {
+        // if the tab is to the left
+        transitionWidth = oldTab.offsetLeft + oldTab.offsetWidth - newTab.offsetLeft;
+        tabsContainer.style.setProperty('--_left', newTab.offsetLeft + "px");
+      }
+      
+      
+      tabsContainer.style.setProperty('--_width', transitionWidth / tabsContainer.offsetWidth);
+
+      setTimeout(() => {
+        tabsContainer.style.setProperty('--_left', newTab.offsetLeft + "px");
+        tabsContainer.style.setProperty('--_width', newTabWidth);
+      }, 240);
+    }
+
+    // Update the indicator when the window is resized
+    function updateIndicator(newTab) {
+      let tabsContainer = document.querySelector('.tabs'); // Select the container
+      const newTabWidth = newTab.offsetWidth / tabsContainer.offsetWidth;
+      tabsContainer.style.setProperty('--_left', newTab.offsetLeft + "px");
+      tabsContainer.style.setProperty('--_width', newTabWidth);
+    }
+
+    // make sure that 'resize' fires only if the current width has crossed the 576px threshold in either direction
+    let previousWidth = window.innerWidth;
+
+    window.addEventListener('resize', () => {
+      const currentWidth = window.innerWidth;
+
+      // Check if the window width has crossed the 576px threshold
+      if ((previousWidth < 576 && currentWidth >= 576) || (previousWidth >= 576 && currentWidth < 576)) {
+        const activeTab = document.querySelector('.tab.active');
+        if (activeTab) {
+          updateIndicator(activeTab);
+        }
+        console.log('resize fired');
+      }
+
+      previousWidth = currentWidth; // Update the previous width for the next resize event
+    });
+   // code that switches between pressed tabs and their content
   
 
   // swiperJS
